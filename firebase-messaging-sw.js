84e97739ecafbe1948eaac898c19d752d9424493
laura53x <laura@familietuerk.de>
2026-03-26 19:06:20 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCUeQ8VaAlOm3dlVvOIzRVAA_GAUCgZnkY",
+  authDomain: "fir-f5586.firebaseapp.com",
+  projectId: "fir-f5586",
+  messagingSenderId: "569764472583",
+  appId: "1:569764472583:web:554781f7e0a3e529ce47a2"
+});
+
+const messaging = firebase.messaging();
